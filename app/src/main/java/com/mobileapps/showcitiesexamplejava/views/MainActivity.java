22f861bf4042a2be904084ada1a6e68601c7d621
Admin <heinerbauza@gmail.com>
2019-09-01 18:56:34 -0400

    set the rv 1st Part

--- app/src/main/java/com/mobileapps/showcitiesexamplejava/views/MainActivity.java
+++ app/src/main/java/com/mobileapps/showcitiesexamplejava/views/MainActivity.java
@@ -1,16 +1,28 @@
 package com.mobileapps.showcitiesexamplejava.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.mobileapps.showcitiesexamplejava.R;
+import com.mobileapps.showcitiesexamplejava.adapter.CitiesAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    CitiesAdapter citiesAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.rvCities);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
