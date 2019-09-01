bc25a6640e4eab13945dc9a1985faf2859ffabce
Admin <heinerbauza@gmail.com>
2019-09-01 18:25:15 -0400

    Create the Model of the objects needed

---
+++ app/src/main/java/com/mobileapps/showcitiesexamplejava/Model/City.java
@@ -0,0 +1,39 @@
+package com.mobileapps.showcitiesexamplejava.Model;
+
+public class City
+{
+    private String country;
+    private Long   _id;
+    private Coord  coord;
+
+    public City(String country, Long _id, Coord coord) {
+        this.country = country;
+        this._id = _id;
+        this.coord = coord;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Long get_id() {
+        return _id;
+    }
+
+    public void set_id(Long _id) {
+        this._id = _id;
+    }
+
+    public Coord getCoord() {
+        return coord;
+    }
+
+    public void setCoord(Coord coord) {
+        this.coord = coord;
+    }
+}
+
